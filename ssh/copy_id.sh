b1f97a214e4404976027d3ce97b636c1647e5ad8
Ivan Rastyapin <i.rastypain@gmail.com>
2020-06-03 02:37:59 +0300

    Add script to prepare SSH

---
+++ ssh/copy_id.sh
@@ -0,0 +1,9 @@
+echo '[SSH] Copy identity...'
+
+if hash sshpass 2>/dev/null; then
+  sshpass -p $DEV_MACHINE_PASSWORD ssh-copy-id -i $DEV_MACHINE_IDENTITY_PATH $DEV_MACHINE_USER@$DEV_MACHINE_HOST
+else
+  ssh-copy-id -i $DEV_MACHINE_IDENTITY_PATH $DEV_MACHINE_USER@$DEV_MACHINE_HOST
+fi
+
+echo '[SSH] Done'
