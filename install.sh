2c4454c798286faa57340cef85d0a149e2c69c0b
Ivan Rastyapin <i.rastypain@gmail.com>
2022-06-17 01:22:21 +0300

    Rework `install.sh`

--- install.sh
+++ install.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
+source './lib/utils'
+
 # Export ENV variables
-ENV_FILE=.env source ./load_env.sh
+dotenv '.env'
+
+scripts_path="./scripts/os/$DEV_MACHINE_OS"
 
 # Prepare OS
-ssh -t $DEV_MACHINE_HOST "$(echo $(< $ENV_FILE)) sh" < ./os/ubuntu.sh
+ssh -t $DEV_MACHINE_HOST "$(echo $(< $CURRENT_ENV_FILE)) sh" < "$scripts_path/prepare_system.sh"
 
-# Prepare SSH
-sh ./ssh/copy_id.sh
+# Create workspaces
+for workspace in "${DEV_MACHINE_WORKSPACES[@]}"; do
+  echo "[Workspace: $workspace] Prepare..."
+  dotenv ".env.$workspace"
 
-# Configure devtools
-ssh -t $DEV_MACHINE_USER@$DEV_MACHINE_HOST "$(echo $(< $ENV_FILE)) sh" < ./configs/devtools.sh
+  # Create user
+  ssh -t $DEV_MACHINE_HOST "$(echo $(< $CURRENT_ENV_FILE)) sh" < "$scripts_path/create_user.sh"
+
+  # Prepare SSH
+  sh ./scripts/copy_identity.sh
+
+  # Configure devtools
+  ssh -t $DEV_MACHINE_USER@$DEV_MACHINE_HOST "$(echo $(< $CURRENT_ENV_FILE)) sh" < "$scripts_path/configure_devtools.sh"
+
+  # Prepare workspace
+  ssh -t $DEV_MACHINE_USER@$DEV_MACHINE_HOST "$(echo $(< $CURRENT_ENV_FILE)) sh" < "$scripts_path/prepare_workspace.sh"
+
+  echo "[Workspace: $workspace] Done"
+done
